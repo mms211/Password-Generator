0e7344c8a90f07a8dd2cc8c1cecb66b8b41d4739
Morgan Sommer <morgana.sommer@gmail.com>
2019-12-01 13:29:04 -0500

    general formatting and all pages created

---
+++ javascript.js
@@ -0,0 +1,20 @@
+// The user will be prompted to choose from the following password criteria:
+
+// * Length (must be between 8 and 128 characters)
+
+//* Character type:
+
+  //* Special characters
+
+  var specialChar = ["~","!","@","#","$","%","^","&","*","(",")","+","=","?","/","{","}","|","<",">",":",";",".",","]
+  //* Numeric characters
+
+  var numeric = [1,2,3,4,5,6,7,8,9,0]
+
+  //* Lowercase characters
+
+  var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+  //* Uppercase characters
+
+  var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
